9d3e2a7fbc73f6ae290381d66d6e8c9a899fc4e0
q4a <q4arus@ya.ru>
2020-04-10 02:51:42 +0300

    fix appveyor checks - part 8

--- src/Application/GameWindowHandler.cpp
+++ src/Application/GameWindowHandler.cpp
@@ -157,7 +157,6 @@ void GameWindowHandler::OnVkDown(int vk, int vk_to_char) {
         if (vk == VK_RETURN) {
             if (!viewparams->field_4C) UI_OnVkKeyDown(vk);
         } else if (vk == VK_CONTROL) {
-            
             bool _507B98_ctrl_pressed = true;
         } else if (vk == VK_ESCAPE) {
             pMessageQueue_50CBD0->AddGUIMessage(UIMSG_Escape, window_SpeakInHouse != 0, 0);
