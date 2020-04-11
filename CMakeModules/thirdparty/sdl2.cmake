0af8281fa3beb16c2bacbb6efc53ce102d604a71
q4a <q4arus@ya.ru>
2020-04-11 12:33:55 +0300

    linux: change cmake files for sdl2 - part2

--- CMakeModules/thirdparty/sdl2.cmake
+++ CMakeModules/thirdparty/sdl2.cmake
@@ -1,16 +1,15 @@
+# hack to be able to create a convenience library without sources
+file(WRITE ${LIB_DIR}/sdl2.c "")
+add_library(
+    sdl2 STATIC
+    ${LIB_DIR}/sdl2.c
+)
+
 if (WIN32)
 set(SDL2_DIR "${LIBRARY_DIR}/sdl2-2.0.12")
 set(SDL2_BIN_DIR "${SDL2_DIR}/bin")
 set(SDL2_LIB_DIR "${SDL2_DIR}/lib")
 set(SDL2_INCLUDE_DIR "${SDL2_DIR}/include")
-set(SDL2_NAME "sdl2")
-
-# hack to be able to create a convenience library without sources
-file(WRITE ${SDL2_DIR}/sdl2.c "")
-add_library(
-    sdl2 STATIC
-    ${SDL2_DIR}/sdl2.c
-)
 
 set_target_properties(
     sdl2 PROPERTIES
@@ -31,5 +30,5 @@ ADD_GLOBAL_DEPENDENCY("${SDL2_BIN_DIR}/SDL2.dll")
 
 else()  # WIN32
 find_package(SDL2 REQUIRED)
-set(SDL2_NAME "SDL2")
+target_link_libraries(sdl2 SDL2)
 endif()
